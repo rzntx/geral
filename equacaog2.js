aa2d872caa674e56e10be7363762078e5fa3d7c2
rzntx <112784744+rzntx@users.noreply.github.com>
2022-09-18 04:07:21 -0300

    Update equacaog2.js

--- equacaog2.js
+++ equacaog2.js
@@ -8,22 +8,29 @@ function eq2() {
         var solucao = (-B - Math.sqrt(delta))/(2 * A);
         var solucao2 = (-B + Math.sqrt(delta))/(2 * A);
         
-        if(delta < 0) {
-            document.getElementById("x1").innerHTML = "Não está definido em R";
-            document.getElementById("x2").innerHTML = "Não está definido em R";
-            document.getElementById("solution").innerHTML = "S = { }";
+        if(A == 0) {
+            document.getElementById("x1").innerHTML = -C/B;
+            document.getElementById("solution").innerHTML = "S = {-C/B }";
         }
         
-        else if(solucao <= solucao2) {
-            document.getElementById("x1").innerHTML = solucao;
-            document.getElementById("x2").innerHTML = solucao2;
-            document.getElementById("solution").innerHTML = "S = { " + solucao + " ; " + solucao2 + " }";
+        else {
+            if(delta < 0) {
+                document.getElementById("x1").innerHTML = "Não está definido em R";
+                document.getElementById("x2").innerHTML = "Não está definido em R";
+                document.getElementById("solution").innerHTML = "S = { }";
+            }
+        
+            else if(solucao <= solucao2) {
+                document.getElementById("x1").innerHTML = solucao;
+                document.getElementById("x2").innerHTML = solucao2;
+                document.getElementById("solution").innerHTML = "S = { " + solucao + " ; " + solucao2 + " }";
+            }
+            else  {
+                document.getElementById("x2").innerHTML = solucao;
+                document.getElementById("x1").innerHTML = solucao2;
+                document.getElementById("solution").innerHTML = "S = { " + solucao2 + " ; " + solucao1 + " }";
         }
-        else  {
-            document.getElementById("x2").innerHTML = solucao;
-            document.getElementById("x1").innerHTML = solucao2;
-            document.getElementById("solution").innerHTML = "S = { " + solucao2 + " ; " + solucao1 + " }";
-    }
+       }
 }
 }
 
